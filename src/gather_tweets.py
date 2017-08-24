6fcbf871e800fab8efe8b46e91d60ecf98755de2
Samuel Ricklin <smrckln@icloud.com>
2017-08-23 20:22:04 -0400

    adding utils

--- src/gather_tweets.py
+++ src/gather_tweets.py
@@ -1,1 +1,11 @@
 import tweepy
+import logging
+
+from .utils import config
+
+if __name__ == '__main__':
+    consumer, consumer_secret, access, access_secret = config._get_twitter_tokens()
+
+
+
+
---
+++ src/utils/config.py
@@ -0,0 +1,13 @@
+import configparser
+
+
+def _get_twitter_tokens():
+    parser = configparser.ConfigParser()
+    parser.read('../creds.conf')
+
+    return (
+        parser['Twitter']['consumer_key'],
+        parser['Twitter']['consumer_secret'],
+        parser['Twitter']['access_token'],
+        parser['Twitter']['access_token_secret']
+    )
